1a24124be1ab9353011b1352bc5a5df30f1f4b74
ISMA3L84 <126180243+ISMA3L84@users.noreply.github.com>
2023-03-21 20:48:19 +0100

    Add files via upload

---
+++ tema7/EjercicioResuelto7_10/Personal/JefeEstacion.java
@@ -0,0 +1,16 @@
+package Personal;
+
+import java.util.Date;
+
+public class JefeEstacion {
+    String nombre;
+    String dni;
+    Date nombramiento;
+
+    public JefeEstacion(String nombre, String dni, Date nombramiento) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.nombramiento = nombramiento;
+    }    
+
+}
---
+++ tema7/EjercicioResuelto7_10/main.java
@@ -0,0 +1,25 @@
+import maquinaria.Tren;
+import maquinaria.Locomotora;
+import Personal.JefeEstacion;
+import Personal.Maquinista;
+import Personal.Mecanico;
+
+
+
+
+
+
+
+public class main {
+private static Object Maquinista;
+
+public static void main(String[] args) {
+    Maquinista casiano = new Maquinista( "Casiano", "12345678", 1500, "3");
+    Maquinista Ambrosio = new Maquinista( "Ambrosio", "45672345", 1300,"2");
+    Mecanico juaneles = new Mecanico("Juaneles",  "12309877", "motor");
+
+    
+}
+
+    }
+    
