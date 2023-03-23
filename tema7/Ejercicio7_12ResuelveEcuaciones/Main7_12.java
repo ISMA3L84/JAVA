00d9a64864d5d3b5f57fd35d9cea97d8f2917a4f
ISMA3L84 <126180243+ISMA3L84@users.noreply.github.com>
2023-03-23 11:57:22 +0100

    Add files via upload

---
+++ tema7/Ejercicio7_12ResuelveEcuaciones/Main7_12.java
@@ -0,0 +1,15 @@
+package ResuelveEcuaciones;
+
+import ResuelveEcuaciones.java.ResuelveEcuaciones;
+
+public class Main7_12 {
+    public static void main(String[]args){
+        ResuelveEcuaciones RE = new ResuelveEcuaciones();
+
+        RE.Ecuacion();
+        RE.Discriminante();
+        
+    }
+
+    
+}
---
+++ tema7/Ejercicio7_12ResuelveEcuaciones/ResuelveEcuaciones.java
@@ -0,0 +1,64 @@
+package ResuelveEcuaciones.java;
+/*Para calcular  las soluciones de una ecuacion de segundo grado hay que 
+ * aplicar una sencilla formula. El único inconveniente es comprobar que
+ * no existan divisiones por 0 o que no calculemos la raiz cuadrada de un
+ * número negativo. Estos errores producen una parada de la ejecucion del
+ * programa.
+ */
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ResuelveEcuaciones{
+    double A, B, C; //coeficientes AX² + BX + C = 0. 
+    double X1, X2, D;// Soluciones y Discriminante.
+    
+    
+    
+    public void Ecuacion(){
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.ITALIAN);
+        double A, B, C; 
+        double X1, X2, D;
+        System.out.println("Introduzca primer coeficiente: (A): ");
+        this.A = sc.nextDouble();
+        System.out.println("Introduzc segundo coeficiente: (B): ");
+        this.B = sc.nextDouble();
+        System.out.println("Introduzca tercer coeficiente: (C): ");
+        this.C = sc.nextDouble();
+        
+
+    }
+    public void Discriminante(){
+        //Calculamos el Discriminante
+        this.D = (B * B - 4 * A * C);
+        if(D < 0){//Como hay que calcular la raiz cuadrada de D, este no
+            //puede ser negativo.
+            System.out.println("No hay solucion posible");
+
+        }else{
+            //Si A = 0 se produce una division por 0. Y en este caso, ni
+            //siquiera sería una ecuación de segundo grado.
+            if (A == 0){
+                System.out.println("No es una ecuacion de segundo grado");
+            }else{
+                this.X1 = (-B + Math.sqrt(D)) / (2 * A);//.srqt() calcula la 
+                //raiz cuadrada.
+                this.X2 = (-B - Math.sqrt(D)) / (2 * A);
+                System.out.println("Solucion 1: " + X1);
+                System.out.println("Solucion 2: " + X2);
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
