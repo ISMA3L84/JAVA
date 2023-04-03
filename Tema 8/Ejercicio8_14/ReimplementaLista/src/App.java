ae472b0bb0cefe883c40043bc9cec35b8947c0e0
ISMA3L84 <126180243+ISMA3L84@users.noreply.github.com>
2023-04-03 21:12:06 +0200

    Add files via upload

---
+++ Tema 8/Ejercicio8_14/ReimplementaLista/src/App.java
@@ -0,0 +1,14 @@
+/*Reimplementa la clase Lista de la actividad resuelta 7.11, sustituyendo
+ * el método mostrar() por el metodo toString();
+ */
+
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Lista L1 = new Lista();
+        Lista L2 = new Lista();
+        L1.toString();
+        L2.toString();
+    }
+}
