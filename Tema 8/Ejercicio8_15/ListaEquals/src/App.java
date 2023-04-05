66053260fcd00350eb1439a84f0fccdd9c92bfcd
ISMA3L84 <126180243+ISMA3L84@users.noreply.github.com>
2023-04-05 17:15:50 +0200

    Add files via upload
    Este ejercicio está sin terminar y da error.

---
+++ Tema 8/Ejercicio8_15/ListaEquals/src/App.java
@@ -0,0 +1,11 @@
+/*Escribe en la clase Lista un método equals() para compararlas. Dos
+ * Listas se considerarán iguales si tienen los mismos elementos (in-
+ * cluídas repeticiones) en el mismo orden.
+ */
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+      
+    }
+}
---
+++ Tema 8/Ejercicio8_15/ListaEquals/src/Lista.java
@@ -0,0 +1,95 @@
+import java.util.Arrays;
+
+
+/*Implementamos las listas con tablas de tipo int, que iremos 
+ * redimensionando según vaya haciendo falta. El índice de un
+ * elemento en la lista, coincide con el índice del lugar que
+ * ocupa en la tabla.
+ */
+public class Lista {
+    Integer[] tabla;
+    Integer[] tabla2;
+    protected int i;
+    
+
+    public Lista(){
+        tabla = new Integer[0];
+        tabla2 = new Integer[0];
+    }
+    void insertarPrincipio(Integer nuevo){
+        tabla = Arrays.copyOf(tabla, tabla.length + 1);
+        tabla[tabla.length -1] = nuevo;
+    }
+
+    void insertarFinal(Integer nuevo){
+        tabla = Arrays.copyOf(tabla, tabla.length + 1);
+        tabla[tabla.length -1] = nuevo;
+    }
+
+    void insertarFinal(Lista otraLista){
+        int tamInicial = tabla.length;//tamaño inicial de la tabla.
+        tabla = Arrays.copyOf(tabla, tabla.length + otraLista.tabla.length);
+        System.arraycopy(otraLista.tabla, 0, tabla, tamInicial, otraLista.tabla.length);
+
+    }
+    //El primer parámetro es el indice del lugar donde queremos in-
+    //sertar el valor del segundo parámetro.
+    void insertar(int posicion, Integer nuevo){
+        tabla = Arrays.copyOf(tabla, tabla.length +1);
+        System.arraycopy(tabla, posicion, tabla, posicion + 1, tabla.length - posicion -1);
+        tabla[posicion] = nuevo;
+    }
+    //Se elimina el elemento correspondiente a indice y se devuelve.
+    Integer eliminar(int indice){
+        Integer eliminado = null;
+        if(indice >= 0 && indice < tabla.length){
+            eliminado = tabla[indice];
+            for(int i = indice + 1 ; i < tabla.length; i++);
+        }
+        return eliminado;
+    }
+    /*Al siguiente método le pasamos un índice y nos devolverá el
+     * elemento correspondiente de la tabla sin modificarla. En el
+     * caso de que el índice no sea válido, devolverá null, con lo
+     * cuál evitamos que el programa aborte.
+     */
+
+     Integer get(int indice){
+        Integer resultado = null;
+        if(indice >= 0 && indice < tabla.length){
+            resultado = tabla[indice];
+        }
+        return resultado;
+     }
+    
+     int buscar(Integer claveBusueda){
+        int indice = -1;
+        for (int i = 0; i < tabla.length && indice == -1; i++);
+        if(tabla[i].equals(claveBusueda)){
+            indice = 1;
+        }
+        return indice;
+
+     }
+     //El numero de elementos de la lista es el numero de elementos de la tabla.
+     public int getnumeroElementos(){
+        return tabla.length;
+     }
+
+     @Override
+     public boolean equals(Object otro){
+        
+        
+        
+
+     }
+
+     
+     //Muestra por consola el contenido de la tabla:
+     public void mostrar(){
+        System.out.println("Lista: " + Arrays.toString(tabla));
+        
+     }
+    
+    
+}
